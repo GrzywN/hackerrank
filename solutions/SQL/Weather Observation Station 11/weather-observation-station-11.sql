e0a32d1a510b13f39bd41c266be6d28f7a9607a2
Karol Binkowski <starystachu9@gmail.com>
2023-03-29 13:20:24 +0200

    feat(SQL): Weather Observation Station 11

---
+++ solutions/SQL/Weather Observation Station 11/weather-observation-station-11.sql
@@ -0,0 +1,19 @@
+SELECT
+  DISTINCT City
+FROM
+  Station
+WHERE
+  (
+    City NOT LIKE 'A%'
+    AND City NOT LIKE 'E%'
+    AND City NOT LIKE 'I%'
+    AND City NOT LIKE 'O%'
+    AND City NOT LIKE 'U%'
+  )
+  OR (
+    City NOT LIKE '%a'
+    AND City NOT LIKE '%e'
+    AND City NOT LIKE '%i'
+    AND City NOT LIKE '%o'
+    AND City NOT LIKE '%u'
+  );
